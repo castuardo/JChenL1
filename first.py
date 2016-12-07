2beec6796db65aaee4b5fdf804d83398d0c4ebda
Jeffrey <jeffreychen51@gmail.com>
2016-12-07 17:08:43 -0600

    This is from this computer

---
+++ first.py
@@ -0,0 +1,12 @@
+#x=1
+#while x < 10:
+#	print "x is" + str(x)
+#	x = x+0.000001
+
+#lst = [1, 2, 3, 4, 5]
+#for element in lst:
+#	print "The element is " + str(element)
+
+#mp = {1 : "number one", 2 : "number two"}
+#for element in mp:
+#	print str(element) + "-->" + str(mp.get(element))
