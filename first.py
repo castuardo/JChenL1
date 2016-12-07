9f277a87f7279f129db68f6759ddea16f01a3d47
Jeffrey <jeffreychen51@gmail.com>
2016-12-07 17:21:39 -0600

    This is the device

--- first.py
+++ first.py
@@ -10,3 +10,16 @@
 #mp = {1 : "number one", 2 : "number two"}
 #for element in mp:
 #	print str(element) + "-->" + str(mp.get(element))
+
+	
+while True:
+	op = raw_input("Do you want to enter a number?: (Yes or No)\n")
+	op = op.lower()
+	if op == "no":
+		print "Goodbye"
+		exit()
+	elif op == "yes":
+		number = raw_input("Enter some number: ")
+		print "You entered " + str(number)
+	else:
+		print "ERROR!?"
